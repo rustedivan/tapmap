5c364999cf2257c40fe31a07576435198029895a
Ivan Milles <ivan@wildbrain.se>
2018-11-20 08:34:21 +0100

    Cleanup: move projection matrix builder to geometry utils

--- tapmap/Geography/MapGeometry.swift
+++ tapmap/Geography/MapGeometry.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import CoreGraphics.CGGeometry
+import GLKit
 
 struct GeometryCounters {
 	static var aabbHitCount = 0
@@ -106,4 +107,28 @@ func triangleSoupHitTest(point p: CGPoint, inVertices vertices: [Vertex], inIndi
 		}
 	}
 	return false
+}
+
+func buildProjectionMatrix(viewSize: CGSize, mapSize: CGSize, centeredOn center: CGPoint, zoomedTo zoom: Float) -> GLKMatrix4 {
+	let projectionMatrix = GLKMatrix4MakeOrtho(0.0, Float(mapSize.width),
+																						 Float(mapSize.height), 0.0,
+																						 0.1, 2.0)
+	let lng = Float((center.x / viewSize.width) * mapSize.width)
+	let lat = Float((center.y / viewSize.height) * mapSize.height)
+	let lngOffset = Float(mapSize.width / 2.0)
+	let latOffset = Float(mapSize.height / 2.0)
+	
+	// Compute the model view matrix for the object rendered with GLKit
+	// (Z = -1.0 to position between the clipping planes)
+	var modelViewMatrix = GLKMatrix4Translate(GLKMatrix4Identity, 0.0, 0.0, -1.0)
+	
+	// Matrix operations, applied in reverse order
+	// 3: Move to scaled UIScrollView content offset
+	modelViewMatrix = GLKMatrix4Translate(modelViewMatrix, -lng, -lat, 0.0)
+	// 2: Scale the data and flip the Y axis
+	modelViewMatrix = GLKMatrix4Scale(modelViewMatrix, zoom, -zoom, 1.0)
+	// 1: Center the data
+	modelViewMatrix = GLKMatrix4Translate(modelViewMatrix, lngOffset, -latOffset, 0.0)
+	
+	return GLKMatrix4Multiply(projectionMatrix, modelViewMatrix)
 }
