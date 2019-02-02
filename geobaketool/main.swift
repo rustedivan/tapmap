12123ff856f9b7fec7def12e37b7781658920ae7
Ivan Milles <ivan@wildbrain.se>
2019-02-02 13:14:15 +0100

    Driver: run all commands so the pipeline can actually be pipeline.

--- geobaketool/main.swift
+++ geobaketool/main.swift
@@ -10,35 +10,38 @@ import Foundation
 
 let commands = CommandLine.arguments.dropFirst()
 
-switch commands.first {
-case "download":
-	do {
-		try downloadFiles(params: commands.dropFirst())
-	} catch GeoBakeDownloadError.timedOut(let host) {
-		print("Connection to \(host) timed out after 60 seconds")
-	} catch GeoBakeDownloadError.downloadFailed(let key) {
-		print("Could not download url referenced in \"\(key)\" in pipeline.json")
-	} catch GeoBakeDownloadError.unpackFailed {
-		print("Could not unzip the downloaded geometry file archive.")
+for command in commands {
+	switch command {
+	case "download":
+		do {
+			try downloadFiles(params: commands.dropFirst())
+		} catch GeoBakeDownloadError.timedOut(let host) {
+			print("Connection to \(host) timed out after 60 seconds")
+		} catch GeoBakeDownloadError.downloadFailed(let key) {
+			print("Could not download url referenced in \"\(key)\" in pipeline.json")
+		} catch GeoBakeDownloadError.unpackFailed {
+			print("Could not unzip the downloaded geometry file archive.")
+		}
+	case "reshape":
+		do {
+			try reshapeGeometry(params: commands.dropFirst())
+		} catch GeoBakeReshapeError.noNodePath {
+			print("No mapshaper path set in pipeline.config. Please set user-relative path to mapshaper in \"reshape.mapshaper\".")
+		} catch GeoBakeReshapeError.noMapshaperInstall {
+			print("No mapshaper install available on PATH. Please run 'npm install -g mapshaper'")
+		} catch GeoBakeReshapeError.missingShapeFile(let level) {
+			print("Could not find a \"\(level)\"-level shapefile. Please re-download.")
+		}
+	case "bake":
+		do {
+			try bakeGeometry()
+		} catch GeoBakePipelineError.datasetFailed(let dataset) {
+			print("Could not bake the \"\(dataset)\" dataset.")
+		} catch {
+			print("Could not bake geometry: \(error.localizedDescription)")
+		}
+		
+	default: print("Usage")
 	}
-case "reshape":
-	do {
-		try reshapeGeometry(params: commands.dropFirst())
-	} catch GeoBakeReshapeError.noNodePath {
-		print("No mapshaper path set in pipeline.config. Please set user-relative path to mapshaper in \"reshape.mapshaper\".")
-	} catch GeoBakeReshapeError.noMapshaperInstall {
-		print("No mapshaper install available on PATH. Please run 'npm install -g mapshaper'")
-	} catch GeoBakeReshapeError.missingShapeFile(let level) {
-		print("Could not find a \"\(level)\"-level shapefile. Please re-download.")
-	}
-case "bake":
-	do {
-		try bakeGeometry()
-	} catch GeoBakePipelineError.datasetFailed(let dataset) {
-		print("Could not bake the \"\(dataset)\" dataset.")
-	} catch {
-		print("Could not bake geometry: \(error.localizedDescription)")
-	}
-	
-default: print("Usage")
 }
+
