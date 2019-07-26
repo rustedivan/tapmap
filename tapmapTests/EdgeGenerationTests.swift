fc1d930dd989065076f699b82560671038c528ca
Ivan Milles <ivan@wildbrain.se>
2019-07-26 11:06:06 +0200

    Prepare for writing edge generation tests

---
+++ tapmapTests/EdgeGenerationTests.swift
@@ -0,0 +1,48 @@
+//
+//  EdgeGenerationTests.swift
+//  tapmapTests
+//
+//  Created by Ivan Milles on 2019-07-22.
+//  Copyright © 2019 Wildbrain. All rights reserved.
+//
+
+import XCTest
+
+class EdgeGenerationTests: XCTestCase {
+
+	func testShouldFattenEdge() {
+		
+	}
+	
+	func shouldIntersectEdges() {
+		
+	}
+	
+	func shouldHandleConvexCorners() {
+		
+	}
+	
+	func shouldHandleIncidentEdges() {
+		
+	}
+	
+	func shouldHandleConcaveCorners() {
+		
+	}
+	
+	func shouldGenerateFatEdgeGeometry() {
+		
+	}
+	
+	func shouldMakeMiterJoin() {
+		
+	}
+	
+	func shouldBuildRenderPrimitive() {
+		
+	}
+	
+	func shouldCacheEdgePrimitives() {
+		
+	}
+}
