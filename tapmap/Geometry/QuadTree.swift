0c8493321a7c162362047c2808a813ff1a5c02d6
Ivan Milles <ivan@wildbrain.se>
2019-12-31 11:45:33 +0100

    Readability fixes(?)

--- tapmap/Geometry/QuadTree.swift
+++ tapmap/Geometry/QuadTree.swift
@@ -45,22 +45,12 @@ struct QuadTree {
 }
 
 func splitBounds(b: Bounds) -> (tl: Bounds, tr: Bounds, bl: Bounds, br: Bounds) {
-	let tlOut = Bounds(minX: b.minX,
-										 maxX: (b.minX + b.maxX) / 2.0,
-										 minY: b.minY,
-										 maxY: (b.minY + b.maxY) / 2.0)
-	let trOut = Bounds(minX: (b.minX + b.maxX) / 2.0,
-										 maxX: b.maxX,
-										 minY: b.minY,
-										 maxY: (b.minY + b.maxY) / 2.0)
-	let blOut = Bounds(minX: b.minX,
-										 maxX: (b.minX + b.maxX) / 2.0,
-										 minY: (b.minY + b.maxY) / 2.0,
-										 maxY: b.maxY)
-	let brOut = Bounds(minX: (b.minX + b.maxX) / 2.0,
-										 maxX: b.maxX,
-										 minY: (b.minY + b.maxY) / 2.0,
-										 maxY: b.maxY)
+	let halfWidth = (b.minX + b.maxX) / 2.0
+	let halfHeight = (b.minY + b.maxY) / 2.0
+	let tlOut = Bounds(minX: b.minX, maxX: halfWidth, minY: b.minY, maxY: halfHeight)
+	let trOut = Bounds(minX: halfWidth, maxX: b.maxX, minY: b.minY, maxY: halfHeight)
+	let blOut = Bounds(minX: b.minX, maxX: halfWidth, minY: halfHeight, maxY: b.maxY)
+	let brOut = Bounds(minX: halfWidth, maxX: b.maxX, minY: halfHeight, maxY: b.maxY)
 	return (tlOut, trOut, blOut, brOut)
 }
 
@@ -76,30 +66,26 @@ func splitNode(_ node: QuadNode) -> QuadNode {
 	}
 }
 
+/// Also need a point-qtree...
+/// Put subcells into an array
 
 func quadInsert(_ value: Int, region: Bounds, into node: QuadNode, depth: Int, maxDepth: Int) -> (QuadNode, Int) {
 	switch (node) {
-	case .Empty(let bounds):
-		let subCells = splitBounds(b: bounds)
-		let splitNode = QuadNode.Node(bounds: bounds, values: [],
-																	tl: .Empty(bounds: subCells.tl),
-																	tr: .Empty(bounds: subCells.tr),
-																	bl: .Empty(bounds: subCells.bl),
-																	br: .Empty(bounds: subCells.br))
-		return quadInsert(value, region: region, into: splitNode, depth: depth, maxDepth: maxDepth)
+	case .Empty:
+		let newNode = splitNode(node)
+		return quadInsert(value, region: region, into: newNode, depth: depth, maxDepth: maxDepth)
 	case .Node(let bounds, var values, var tl, var tr, var bl, var br):
 		let nextDepth = depth + 1
 		if (nextDepth <= maxDepth) {
+			func insertValueInto(_ target: QuadNode) -> (QuadNode, Int) {
+				quadInsert(value, region: region, into: target, depth: nextDepth, maxDepth: maxDepth)
+			}
 			let newDepth: Int
-			if (tl.contains(region: region)) {
-				(tl, newDepth) = quadInsert(value, region: region, into: tl, depth: nextDepth, maxDepth: maxDepth)
-			} else if (tr.contains(region: region)) {
-				(tr, newDepth) = quadInsert(value, region: region, into: tr, depth: nextDepth, maxDepth: maxDepth)
-			} else if (bl.contains(region: region)) {
-				(bl, newDepth) = quadInsert(value, region: region, into: bl, depth: nextDepth, maxDepth: maxDepth)
-			} else if (br.contains(region: region)) {
-				(br, newDepth) = quadInsert(value, region: region, into: br, depth: nextDepth, maxDepth: maxDepth)
-			} else {
+			if      (tl.contains(region: region)) { (tl, newDepth) = insertValueInto(tl)	}
+			else if (tr.contains(region: region)) { (tr, newDepth) = insertValueInto(tr) }
+			else if (bl.contains(region: region)) { (bl, newDepth) = insertValueInto(bl) }
+			else if (br.contains(region: region)) { (br, newDepth) = insertValueInto(br) }
+			else {
 				newDepth = depth
 				values.append(value)
 			}
