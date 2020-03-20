3e351a0e08ec53fec0046c6ad62d6400c57d463f
Ivan Milles <ivan@wildbrain.se>
2020-03-20 13:19:43 +0100

    Fix label promotion

--- geobaketool/Operations/OperationFixupHierarchy.swift
+++ geobaketool/Operations/OperationFixupHierarchy.swift
@@ -44,7 +44,8 @@ class OperationFixupHierarchy : Operation {
 			// ...and all the larger places in those regions
 			let belongingPlaces = Set(belongingRegions
 				.flatMap { $0.places ?? [] }
-				.filter { $0.kind != .Town }
+				.filter { $0.kind != .Town }	// Don't promote towns and region labels
+				.filter { $0.kind != .Region }
 			)
 			
 			var updatedCountry = country
@@ -74,7 +75,7 @@ class OperationFixupHierarchy : Operation {
 			
 			var updatedContinent = continent
 			updatedContinent.children = belongingCountries
-			updatedContinent.places = belongingPlaces
+			updatedContinent.places = (continent.places ?? Set()).union(belongingPlaces)
 			geoContinents.insert(updatedContinent)
 			
 			remainingCountries.subtract(belongingCountries)
