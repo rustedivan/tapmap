193224fdd866bc7bba66d3e8248d84fc8584d8f2
Ivan Milles <ivan@wildbrain.se>
2020-04-18 12:19:39 +0200

    Accessors to replace UserState's accessors

--- tapmap/App/RuntimeWorld.swift
+++ tapmap/App/RuntimeWorld.swift
@@ -26,4 +26,34 @@ class RuntimeWorld {
 		let regionList = countryList.flatMap { $0.children }
 		allRegions = Dictionary(uniqueKeysWithValues: regionList.map { ($0.geographyId.hashed, $0) })
 	}
+	
+	var availableContinents: [Int: GeoContinent] { get {
+		let user = AppDelegate.sharedUserState
+		return allContinents.filter { user.availableContinents.contains($0.key) }
+	}}
+	
+	var availableCountries: [Int: GeoCountry] { get {
+		let user = AppDelegate.sharedUserState
+		return allCountries.filter { user.availableCountries.contains($0.key) }
+	}}
+	
+	var availableRegions: [Int: GeoRegion] { get {
+		let user = AppDelegate.sharedUserState
+		return allRegions.filter { user.availableRegions.contains($0.key) }
+	}}
+	
+	var visibleContinents: [Int: GeoContinent] { get {
+		let ui = AppDelegate.sharedUIState
+		return availableContinents.filter { ui.visibleRegionHashes.contains($0.key) }
+	}}
+	
+	var visibleCountries: [Int: GeoCountry] { get {
+		let ui = AppDelegate.sharedUIState
+		return availableCountries.filter { ui.visibleRegionHashes.contains($0.key) }
+	}}
+	
+	var visibleRegions: [Int: GeoRegion] { get {
+		let ui = AppDelegate.sharedUIState
+		return availableRegions.filter { ui.visibleRegionHashes.contains($0.key) }
+	}}
 }
