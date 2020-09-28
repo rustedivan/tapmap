5b123b086683ada44fb0d7ce45f5c508fdffcbbe
Ivan Milles <ivan@rusted.se>
2020-09-28 22:57:29 +0200

    Setup color fixables

--- tapmap/App/AppDelegate.swift
+++ tapmap/App/AppDelegate.swift
@@ -15,23 +15,29 @@ struct AppFixables {
 	static let continentBorderOuter = FixableId()
 	static let countryBorderInner = FixableId()
 	static let countryBorderOuter = FixableId()
+	static let oceanColor = FixableId()
+	static let continentColor = FixableId()
+	static let unvisitedCountryColor = FixableId()
+	static let unvisitedCountryBorderColor = FixableId()
+	static let visitedCountryColor = FixableId()
+	static let visitedCountryBorderColor = FixableId()
 }
-
-//FixableSetup("Continent inside", config: .float(value: 0.1, min: 0.0, max: 2.0))
-//FixableSetup("Continent outside", config: .float(value: 0.1, min: 0.0, max: 2.0))
-//FixableSetup("Country inside", config: .float(value: 0.1, min: 0.0, max: 2.0))
-//FixableSetup("Country outside", config: .float(value: 0.1, min: 0.0, max: 2.0))
-
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 	var fixaStream = FixaStream(fixableSetups: [
-		FixableId() :											 .divider(display: FixableDisplay("Continents")),
-		AppFixables.continentBorderInner : .float(value: 0.1, min: 0.0, max: 2.0, display: FixableDisplay("Border inside")),
-		AppFixables.continentBorderOuter : .float(value: 0.1, min: 0.0, max: 2.0, display: FixableDisplay("Border outside")),
-		FixableId() : 										 .divider(display: FixableDisplay("Countries")),
-		AppFixables.countryBorderInner : 	 .float(value: 0.1, min: 0.0, max: 2.0, display: FixableDisplay("Border inside")),
-		AppFixables.countryBorderOuter : 	 .float(value: 0.1, min: 0.0, max: 2.0, display: FixableDisplay("Border inside"))
+		FixableId() :											 	.divider(display: FixableDisplay("Continents")),
+		AppFixables.continentBorderInner : 	.float(value: 0.1, min: 0.0, max: 2.0, display: FixableDisplay("Border inside")),
+		AppFixables.continentBorderOuter : 	.float(value: 0.1, min: 0.0, max: 2.0, display: FixableDisplay("Border outside")),
+		AppFixables.oceanColor:						 	.color(value: UIColor.blue.cgColor, display: FixableDisplay("Ocean")),
+		AppFixables.continentColor:				 	.color(value: UIColor.green.cgColor, display: FixableDisplay("Continent")),
+		FixableId() : 										 	.divider(display: FixableDisplay("Countries")),
+		AppFixables.countryBorderInner : 	 	.float(value: 0.1, min: 0.0, max: 2.0, display: FixableDisplay("Border inside")),
+		AppFixables.countryBorderOuter : 	 	.float(value: 0.1, min: 0.0, max: 2.0, display: FixableDisplay("Border inside")),
+		AppFixables.unvisitedCountryColor: 			 .color(value: UIColor.red.cgColor, display: FixableDisplay("Unvisited")),
+		AppFixables.unvisitedCountryBorderColor: .color(value: UIColor.yellow.cgColor, display: FixableDisplay("Unvisited border")),
+		AppFixables.visitedCountryColor:				 .color(value: UIColor.cyan.cgColor, display: FixableDisplay("Visited")),
+		AppFixables.visitedCountryBorderColor: 	 .color(value: UIColor.magenta.cgColor, display: FixableDisplay("Visited border")),
 	])
 	
 	var window: UIWindow?
--- tapmap/Rendering/StyleSheet.swift
+++ tapmap/Rendering/StyleSheet.swift
@@ -7,16 +7,25 @@
 //
 
 import Foundation
-
+import UIKit.UIColor
 import fixa
 
 class Stylesheet {
 	static let shared = Stylesheet()
 	
+	// Border widths
 	var continentBorderWidthInner = FixableFloat(AppFixables.continentBorderInner, initial: 0.1)
 	var continentBorderWidthOuter = FixableFloat(AppFixables.continentBorderOuter, initial: 0.1)
 	var countryBorderWidthInner = FixableFloat(AppFixables.countryBorderInner, initial: 0.1)
 	var countryBorderWidthOuter = FixableFloat(AppFixables.countryBorderOuter, initial: 0.1)
 	var provinceBorderWidthInner: Float = 0.3
 	var provinceBorderWidthOuter: Float = 0.1
+	
+	// Map colors
+	var oceanColor = FixableColor(AppFixables.oceanColor, initial: UIColor.blue.cgColor)
+	var continentColor = FixableColor(AppFixables.continentColor, initial: UIColor.green.cgColor)
+	var unvisitedCountryColor = FixableColor(AppFixables.unvisitedCountryColor, initial: UIColor.red.cgColor)
+	var unvisitedCountryBorderColor = FixableColor(AppFixables.unvisitedCountryBorderColor, initial: UIColor.yellow.cgColor)
+	var visitedCountryColor = FixableColor(AppFixables.visitedCountryColor, initial: UIColor.cyan.cgColor)
+	var visitedCountryBorderColor = FixableColor(AppFixables.visitedCountryBorderColor, initial: UIColor.magenta.cgColor)
 }
