918562ff3e367362816f2e516860448145d44679
Ivan Milles <ivan@rusted.se>
2021-01-24 22:45:19 +0100

    Fix box layout, sigh

--- tapmap/Mapping/GeoRegion.swift
+++ tapmap/Mapping/GeoRegion.swift
@@ -75,7 +75,7 @@ struct Aabb : Equatable, Codable {
 	}
 	
 	var width: Vertex.Precision { return maxX - minX }
-	var height: Vertex.Precision { return maxX - minX }
+	var height: Vertex.Precision { return maxY - minY }
 	var midpoint: Vertex { return Vertex(Vertex.Precision(minX + ((maxX - minX) / 2.0)),
 																			 Vertex.Precision(minY + ((maxY - minY) / 2.0))) }
 	
