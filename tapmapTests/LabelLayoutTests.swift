b59c19c8df6dc9a4729883dd3a45bbdec04fd50a
Ivan Milles <ivan@rusted.se>
2021-01-30 22:04:21 +0100

    Finish layout unit tests

--- tapmapTests/LabelLayoutTests.swift
+++ tapmapTests/LabelLayoutTests.swift
@@ -21,6 +21,10 @@ func offsetProjection(v: Vertex) -> CGPoint {
 	return CGPoint(x: CGFloat(v.x + 4.0), y: CGFloat(v.y - 5.0))
 }
 
+func zoomProjection(v: Vertex) -> CGPoint {
+	return CGPoint(x: CGFloat(v.x * 0.5), y: CGFloat(v.y * 0.5))
+}
+
 
 func makeMarkers(_ places: [GeoPlace]) -> [Int : LabelMarker] {
 	return Dictionary(uniqueKeysWithValues: places.map { ($0.hashValue, LabelMarker(for: $0)) })
@@ -92,7 +96,6 @@ class LabelLayoutTests: XCTestCase {
 			GeoPlace(location: Vertex(12, 10), name: "4: North-West", kind: .City, rank: 3),
 			GeoPlace(location: Vertex(12, 12), name: "5: South-West", kind: .City, rank: 4),
 			GeoPlace(location: Vertex(11, 11), name: "No marker fit", kind: .City, rank: 5),
-			
 		])
 		
 		let (layout, _) = layouter.layoutLabels(markers: markers, projection: nullProjection)
@@ -135,30 +138,66 @@ class LabelLayoutTests: XCTestCase {
 	}
 	
 	func testNewMarkerPrioritization() {
+		let markers = makeMarkers([
+			GeoPlace(location: Vertex(5, 70), name: "Marker #6", kind: .Town, rank: 6),
+			GeoPlace(location: Vertex(5, 70), name: "Marker #5", kind: .Town, rank: 5),
+			GeoPlace(location: Vertex(5, 70), name: "Marker #4", kind: .Town, rank: 4),
+			GeoPlace(location: Vertex(5, 50), name: "Marker #3", kind: .City, rank: 3),
+			GeoPlace(location: Vertex(5, 30), name: "Marker #2", kind: .Capital, rank: 2),
+			GeoPlace(location: Vertex(5, 10), name: "Marker #1", kind: .Region, rank: 1),
+		])
 		
-	}
-	
-	func testLabelLimit() {
-		
-	}
-	
-	func testLayoutWithAnchorSequence() {
-		
-	}
-	
-	func testLayoutRejectedLabel() {
-		
+		_ = layouter.layoutLabels(markers: markers, projection: nullProjection)
+		XCTAssertEqual(layouter.orderedLayout.count, 5)	// 6th label rejected due to maxLabels
+		XCTAssertEqual(layouter.orderedLayout[0].debugName, "Marker #1")
+		XCTAssertEqual(layouter.orderedLayout[1].debugName, "Marker #2")
+		XCTAssertEqual(layouter.orderedLayout[2].debugName, "Marker #3")
+		XCTAssertEqual(layouter.orderedLayout[3].debugName, "Marker #4")
+		XCTAssertEqual(layouter.orderedLayout[4].debugName, "Marker #5")
 	}
 	
 	func testEjectedLabel() {
+		let markers = makeMarkers([
+			GeoPlace(location: Vertex(10, 10), name: "Marker A", kind: .City, rank: 1),
+			GeoPlace(location: Vertex(32, 10), name: "Marker B", kind: .City, rank: 2),
+			GeoPlace(location: Vertex(32, 32), name: "Marker C", kind: .City, rank: 3),
+			GeoPlace(location: Vertex(10, 32), name: "Marker D", kind: .City, rank: 4),
+			GeoPlace(location: Vertex(25, 25), name: "X", kind: .Town, rank: 5),
+		])
 		
+		let (_, removed1) = layouter.layoutLabels(markers: markers, projection: nullProjection)
+		XCTAssertEqual(layouter.orderedLayout.count, 5)
+		XCTAssertEqual(removed1.count, 0)
+		
+		let (_, removed2) = layouter.layoutLabels(markers: markers, projection: zoomProjection)
+		XCTAssertEqual(layouter.orderedLayout.count, 4)
+		XCTAssertEqual(removed2.count, 1)
+		XCTAssertEqual(markers[removed2[0]]!.name, "X")
 	}
 	
 	func testRemoveFromLayout() {
+		let markers = makeMarkers([
+			GeoPlace(location: Vertex(5, 5), name: "Keep", kind: .City, rank: 1),
+			GeoPlace(location: Vertex(5, 35), name: "Remove", kind: .City, rank: 2)
+		])
 		
+		_ = layouter.layoutLabels(markers: markers, projection: nullProjection)
+		layouter.removeLayout(for: markers.first!.value.ownerHash)
+		XCTAssertEqual(layouter.orderedLayout.count, 1)
 	}
 	
 	func testInsertionHysteresis() {
+		layouter.labelMargin = 5.0
+		let markers = makeMarkers([
+			GeoPlace(location: Vertex(20, 20), name: "1: Upper", kind: .Region, rank: 1),
+			GeoPlace(location: Vertex(20, 20 + 8 + 2), name: "2: In margin", kind: .Region, rank: 2),
+			GeoPlace(location: Vertex(20, 20 + 8 + 5), name: "3: Outside margin", kind: .Region, rank: 2),
+		])
 		
+		let (layout, _) = layouter.layoutLabels(markers: markers, projection: nullProjection)
+		let ls = prepareForAsserts(layout)
+		XCTAssertEqual(ls.count, 2)
+		XCTAssertEqual(ls[0].debugName, "1: Upper")
+		XCTAssertEqual(ls[1].debugName, "3: Outside margin")
 	}
 }
