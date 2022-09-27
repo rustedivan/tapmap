fe00a8cc0f8bdbec54567325a8b45be1fb39f1d9
Ivan Milles <ivan@rusted.se>
2022-09-27 08:49:24 +0200

    Cleanup

--- tapmap/App/MapViewController.swift
+++ tapmap/App/MapViewController.swift
@@ -76,7 +76,6 @@ class MapViewController: UIViewController, MTKViewDelegate {
 		super.viewDidLoad()
 		
 		let metalView = view as! MTKView
-//		metalView.sampleCount = 4
 		renderers = MetalRenderer(in: metalView, forWorld: world)
 		geometryStreamer.metalDevice = renderers.device
 		metalView.delegate = self
